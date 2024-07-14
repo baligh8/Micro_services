b75c1227afda38d1397ea832182caab954380ada
baligh8 <obaligh8@gmail.com>
2024-07-14 17:16:14 +0100

    r

--- gateway-server/src/main/java/com/example/gateway_server/GatewayServerApplication.java
+++ gateway-server/src/main/java/com/example/gateway_server/GatewayServerApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GatewayServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServerApplication.class, args);
 	}
